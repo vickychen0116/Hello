dae28be9ad6b3ac086ad16a04a0ff5181ac211e7
vicky <vickychen0116@gmail.com>
2020-07-29 16:03:13 +0800

    初始

---
+++ src/routes/api.ts
@@ -0,0 +1,28 @@
+
+var express = require('express');
+var app     = express();
+var port    = process.env.PORT || 3000;
+var router = express.Router();
+
+// 在每一個請求被處理之前都會執行的 middleware
+router.use(function(req:string, res:any, next) {
+
+  // 輸出記錄訊息至終端機
+  console.log(req.method, req.url);
+
+  // 繼續路由處理
+  next();
+});
+
+app.get('/', function(req:string, res:any) {
+  res.send('Hello World!');
+});
+app.get('/api', function (req:string, res:any) {
+  res.json({ message: "第一個API!" });
+});
+
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
